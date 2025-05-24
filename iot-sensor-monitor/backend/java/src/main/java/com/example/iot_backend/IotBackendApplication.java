d71157dda6f2e4179b59c43d294f7ade0d80696d
Josué KOFFI <josue-kinsanh-nixxon.koffi@efrei.net>
2025-05-24 12:43:54 +0200

    Update IotBackendApplication.java

--- iot-sensor-monitor/backend/java/src/main/java/com/example/iot_backend/IotBackendApplication.java
+++ iot-sensor-monitor/backend/java/src/main/java/com/example/iot_backend/IotBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.iot_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;   // <-- Add this import!
 
 @SpringBootApplication
+@EnableScheduling  // <-- Add this annotation!
 public class IotBackendApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class IotBackendApplication {
 	}
 
 }
+
